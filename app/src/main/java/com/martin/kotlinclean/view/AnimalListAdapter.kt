729aa834fba1304c0ed1f7952cf6e6350ea905a2
MartinGelevskiCodewell <martin.gelevski@codewell.mk>
2019-11-28 00:35:30 +0100

    Implementing data binding into the xml

--- app/src/main/java/com/martin/kotlinclean/view/AnimalListAdapter.kt
+++ app/src/main/java/com/martin/kotlinclean/view/AnimalListAdapter.kt
@@ -1,21 +1,20 @@
 package com.martin.kotlinclean.view
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.martin.kotlinclean.R
+import com.martin.kotlinclean.databinding.ItemAnimalBinding
 import com.martin.kotlinclean.model.Animal
-import com.martin.kotlinclean.util.getProgressDrawable
-import com.martin.kotlinclean.util.loadImage
 import kotlinx.android.synthetic.main.item_animal.view.*
 
 class AnimalListAdapter(private val animlList: ArrayList<Animal>) :
     RecyclerView.Adapter<AnimalListAdapter.AnimalViewHolder>() {
 
 
-    fun updateAnimalList(newAnimalList: List<Animal>){
+    fun updateAnimalList(newAnimalList: List<Animal>) {
         animlList.clear()
         animlList.addAll(newAnimalList)
         notifyDataSetChanged()
@@ -25,7 +24,7 @@ class AnimalListAdapter(private val animlList: ArrayList<Animal>) :
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AnimalViewHolder {
 
         val inflater = LayoutInflater.from(parent.context)
-        val view = inflater.inflate(R.layout.item_animal, parent, false)
+        val view = DataBindingUtil.inflate<ItemAnimalBinding>(inflater, R.layout.item_animal, parent, false)
 
         return AnimalViewHolder(view)
 
@@ -36,21 +35,17 @@ class AnimalListAdapter(private val animlList: ArrayList<Animal>) :
 
 
     override fun onBindViewHolder(holder: AnimalViewHolder, position: Int) {
-        holder.itemView.animalName.text = animlList[position].name
-        holder.itemView.animal_image.loadImage(animlList[position].imageUrl, getProgressDrawable(holder.itemView.context))
 
+        holder.view.animal = animlList[position]
         holder.itemView.animalLayout.setOnClickListener {
-            val action= ListFragmentDirections.actionDetail(animlList[position])
+            val action = ListFragmentDirections.actionDetail(animlList[position])
 
             Navigation.findNavController(holder.itemView).navigate(action)
         }
     }
 
 
-    class AnimalViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-
-    }
+    class AnimalViewHolder(var view: ItemAnimalBinding) : RecyclerView.ViewHolder(view.root)
 
 
 }
--- app/src/main/java/com/martin/kotlinclean/view/DetailFragment.kt
+++ app/src/main/java/com/martin/kotlinclean/view/DetailFragment.kt
@@ -5,51 +5,28 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
 import com.martin.kotlinclean.R
+import com.martin.kotlinclean.databinding.FragmentDetailBinding
 import com.martin.kotlinclean.model.Animal
-import com.martin.kotlinclean.util.getProgressDrawable
-import com.martin.kotlinclean.util.loadImage
-import kotlinx.android.synthetic.main.fragment_detail.*
 
-/**
- * A simple [Fragment] subclass.
- */
 class DetailFragment : Fragment() {
+    var animal: Animal? = null
+
+    private lateinit var dataBinding: FragmentDetailBinding
 
 
-    var animal: Animal?  = null
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        dataBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_detail, container, false)
+        return dataBinding.root
     }
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-
-        arguments?.let {
-            animal= DetailFragmentArgs.fromBundle(it).animal
-        }
-
-        context?.let {
-            animalImage.loadImage(animal?.imageUrl, getProgressDrawable(it))
-
-        }
-
-        animalName.text= animal?.name
-        animalLocation.text= animal?.location
-        animalLifespan.text= animal?.lifeSpan
-        animalDiet.text= animal?.diet
-
-
-
+        arguments?.let { animal = DetailFragmentArgs.fromBundle(it).animal }
+        dataBinding.animal = animal
     }
 
 
